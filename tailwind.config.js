229c1b56e7004f87e7ee0d4d61da09d2a7a8ace9
dev_nayan <md.nayan8494@gmail.com>
2022-06-03 00:32:26 +0600

    Initialize the project and create routes

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#0fcfec',
+
+          secondary: '#19d3ae',
+
+          accent: '#3a4256',
+
+          'base-100': '#FFFFFF',
+
+          info: '#3ABFF8',
+
+          success: '#36D399',
+
+          warning: '#FBBD23',
+
+          error: '#F87272',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+}
