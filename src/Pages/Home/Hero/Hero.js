1980db976577d484c83ea135ca7cc0e3c527fbc6
dev_nayan <md.nayan8494@gmail.com>
2022-06-05 01:18:10 +0600

    Hero section responsive issues fixed

--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -4,11 +4,11 @@ import PrimaryBtn from '../../Shared/PrimaryBtn';
 
 const Hero = () => {
     return (
-        <div className="hero min-h-screen lg:bg-hero-pattern hero-section">
+        <div className="hero py-1 lg:py-5 lg:min-h-screen lg:bg-hero-pattern hero-section">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img className='lg:w-2/4' src={heroImg} alt='Hero background'/>
                 <div>
-                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <h1 className="text-4xl mt-4 lg:mt-0 lg:text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
                     <PrimaryBtn>Get Started</PrimaryBtn>
                 </div>
