a258b56518f94464399487df69306feb0aafffb8
dev_nayan <md.nayan8494@gmail.com>
2022-06-10 20:55:08 +0600

    Google Signin method applied using react-firebase-hook

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
 
 const Login = () => {
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+
+    console.log(user);
+
     return (
-        <div>
-            <h2>Login</h2>
+        <div className='h-full flex justify-center items-center py-20'>
+            <div className="card w-96 bg-base-100 shadow-lg">
+            <div className="card-body">
+                <h2 className="text-2xl font-bold text-center text-secondary">Login</h2>
+                <div className="divider">OR</div>
+                <button onClick={() => signInWithGoogle()} className="btn border-secondary text-secondary hover:bg-secondary hover:border-secondary hover:text-white">Continue with google</button>
+            </div>
+            </div>
         </div>
     );
 };
